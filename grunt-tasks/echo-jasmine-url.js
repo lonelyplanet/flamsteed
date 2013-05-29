2d5b83bb92ae5a2e1f4c9518240e32240181b7f1
Remy Bach <remy@bach.me.uk>
2013-05-29 16:56:46 +0100

    Colourize the url in the echoJasmineUrl task.

--- grunt-tasks/echo-jasmine-url.js
+++ grunt-tasks/echo-jasmine-url.js
@@ -2,6 +2,6 @@ module.exports = function(grunt) {
 	'use strict';
 
 	grunt.registerTask('echoJasmineUrl', 'Literally just spit out a url to the jasmine specrunner file.', function() {
-		grunt.log.writeln('You can run your tests at the following url: http://localhost:8888/_SpecRunner.html');
+		grunt.log.writeln('You can run your tests at the following url: \u001b[0;33mhttp://localhost:8888/_SpecRunner.html\u001b[0m');
 	});
 };
